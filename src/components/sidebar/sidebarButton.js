c18abf028b103bba735ea03a66bce8433f376add
XTS <johndoe@example.com>
2022-12-09 14:46:34 +0300

    Intermedia commit

--- src/components/sidebar/sidebarButton.js
+++ src/components/sidebar/sidebarButton.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { IconContext } from 'react-icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './sidebarButton.css';
 
 export default function SidebarButton(props) {
+    const location = useLocation();
+
+    const isActive = location.pathname === props.to;
+    const btnClass = isActive ? "btn-body active" : "btn-body";
     return (
         <Link to={props.to}>
-            <div className="btn-body">
+            <div className="{btnClass}">
                 <IconContext.Provider value={{ size: '24px', className: 'bnt-icon' }}>
                     {props.icon}
                     <p className="btn-title"> {props.title} </p>
