0c9e2c2c5f82cd7513bc9480f5506e054f04812c
XTS <johndoe@example.com>
2022-12-09 11:51:36 +0300

    Intermedia commit build site-bar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './sidebar.css';
+import SidebarButton from './sidebarButton';
+import { MdFavorite } from 'react-icons/md';
+import { FaGripfire, FaPlay } from 'react-icons/fa';
+import { FaSignOutAlt } from 'react-icons/fa';
+import { IoLibrary } from 'react-icons/io5';
+import { MdSpaceDashboard } from 'react-icons/md';
 
 export default function SideBar() {
-    return <div className="sidebar-container">
-        <img src="https://texterra.ru/upload/medialibrary/c50/y0wypgv33kct9i82jihoz0v3n0zp40al/2.webp"
-            className="profile-img"
-            alt="profile" />
-    </div>
+    return (
+        <div className="sidebar-container">
+            <img
+                src="https://texterra.ru/upload/medialibrary/c50/y0wypgv33kct9i82jihoz0v3n0zp40al/2.webp"
+                className="profile-img"
+                alt="profile"
+            />
+            <div>
+                <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+                <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+                <SidebarButton title="Playre" to="/player" icon={<FaPlay />} />
+                <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+                <SidebarButton title="Library" to="/library" icon={<IoLibrary />} />
+            </div>
+            <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
+        </div>
+    );
 }
---
+++ src/components/sidebar/sidebarButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './sidebarButton.css';
+
+export default function SidebarButton(props) {
+    return (
+        <Link to={props.to}>
+            <div>
+                {props.icon}
+                <p> {props.title} </p>
+            </div>
+        </Link>
+    );
+}
